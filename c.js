d4dff36704c7b53e742408c5875f3fb583f13cc8
Animesh Kumar Sharma <60816072+Animesh191@users.noreply.github.com>
2021-09-24 13:06:45 +0530

    Update c.js

--- c.js
+++ c.js
@@ -9,7 +9,7 @@ let output = document.querySelector('#output');
 let refreshButton = document.querySelector('#refreshButton');
 let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let emptyArr = [];
-for (let i = 1; i <= 7; i++) {
+for (let i = 1; i <= 4; i++) {
  emptyArr.push(alphaNums[Math.floor(Math.random() * alphaNums.length)]);
 }
 var c = emptyArr.join('');
